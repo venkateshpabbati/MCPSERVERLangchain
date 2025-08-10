e443aea4eb7801252001ebec8aa3d9151eae792d
VenkateshPabbati <venkateshpabbati@outlook.com>
2025-08-10 18:14:00 +0530

    multiple ot multiply

--- mathserver.py
+++ mathserver.py
@@ -1,22 +1,19 @@
 from mcp.server.fastmcp import FastMCP
 
-mcp=FastMCP("Math")
+mcp = FastMCP("Math")
 
 @mcp.tool()
-def add(a:int,b:int)->int:
-    """_summary_
-    Add to numbers
-    """
-    return a+b
+def add(a: int, b: int) -> int:
+    """Add two numbers and return the result."""
+    return a + b
 
 @mcp.tool()
-def multiple(a:int,b:int)-> int:
-    """Multiply two numbers"""
-    return a*b
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers and return the result."""
+    return a * b
 
-#The transport="stdio" argument tells the server to:
+# The transport="stdio" argument tells the server to:
+# Use standard input/output (stdin and stdout) to receive and respond to tool function calls.
 
-#Use standard input/output (stdin and stdout) to receive and respond to tool function calls.
-
-if __name__=="__main__":
+if __name__ == "__main__":
     mcp.run(transport="stdio")
